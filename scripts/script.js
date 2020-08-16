897f59c56f9cc70905909f5838c13842b7e90425
Mark <mbenneth7@yahoo.com>
2020-08-16 11:37:21 -0400

    updated code to play best out of 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
-let computerChoice = computerAction();
-let userChoice = userAction();
+let userWins = 0;
+let compWins = 0;
+let limit = 0;
+let computerChoice = "";
+let userChoice = "";
+
+
 
 function computerAction(){
     let choice = Math.ceil(Math.random() * 3);
@@ -22,47 +27,89 @@ function computerAction(){
 
 function userAction(){
 
-    let userChoice = prompt("Pick Rock Paper or Scissors");
+    let userPrompt = prompt("Pick Rock Paper or Scissors");
 
-    if(userChoice === ""){
+    if(userPrompt === ""){
         console.log("Okay we are going to pick rock paper or scissors for you");
         let choice = Math.ceil(Math.random() * 3);
 
         switch(choice){
 
             case 1:
-                userChoice = "rock";
+                userPrompt = "rock";
                 break;
             case 2: 
-                userChoice = "paper";
+                userPrompt = "paper";
                 break;    
             case 3: 
-                userChoice = "scissors";
+                userPrompt = "scissors";
                 break;
         }
 
     }
     else
-        userChoice =  userChoice.toLowerCase();
-    
-    return userChoice;
+        userPrompt =  userPrompt.toLowerCase();
+
+    return userPrompt;
 
 }
 
 function playRound(userChoice, computerChoice){
+
     console.log(`User's Choice: ${userChoice}`);
     console.log(`Computer's Choice: ${computerChoice}`);
 
-    if(computerChoice === "scissors" && userChoice === "paper") console.log("Computer Wins");
-    else if(computerChoice === "rock" && userChoice === "scissors") console.log("Computer Wins");
-    else if(computerChoice === "paper" && userChoice === "rock") console.log("Computer Wins");
-    else if(userChoice === "scissors" && computerChoice === "paper") console.log("User Wins");
-    else if(userChoice === "paper" && computerChoice === "rock") console.log("User Wins");
-    else if(userChoice === "rock" && computerChoice === "scissors") console.log("User Wins");
-    else console.log("It's a draw");
+    if(computerChoice === "scissors" && userChoice === "paper"){ 
+        console.log("Computer Wins");
+        compWins++;
+    }
+    else if(computerChoice === "rock" && userChoice === "scissors"){
+        console.log("Computer Wins");
+        compWins++;
+    }
+    else if(computerChoice === "paper" && userChoice === "rock"){ 
+        console.log("Computer Wins");
+        compWins++;
+    }
+    else if(userChoice === "scissors" && computerChoice === "paper"){ 
+        console.log("User Wins");
+        userWins++;
+    }
+    else if(userChoice === "paper" && computerChoice === "rock"){ 
+        console.log("User Wins");
+        userWins++;
+    }    
+    else if(userChoice === "rock" && computerChoice === "scissors"){
+        console.log("User Wins");
+        userWins++;
+    }
+    else
+        console.log("It's a draw"); 
+}
+
+
+
+function game(){
+    
+    while(limit < 5){
+        userChoice = userAction();
+        computerChoice = computerAction();
+        playRound(userChoice, computerChoice);
+        console.log(`User Wins: ${userWins}`);
+        console.log(`Comp Wins: ${compWins}`);
+        limit++;
+
+    }
+
+    if(userWins > compWins)
+        console.log("User wins the WAR!!!!");
+    else
+        console.log("The day belongs to the MACHIINES!!!!");     
 
 }
 
-playRound(userChoice, computerChoice);
+game();
+
+
 
 
