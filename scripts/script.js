9cf5ede184c5bbdcdaca4870b7d77a2342839d64
Mark <mbenneth7@yahoo.com>
2020-08-27 10:37:26 -0400

    added simple user UI

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ let compWins = 0;
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
+let display = document.querySelector("#display");
 let userChoice = "";
 let computerChoice = "";
 
@@ -59,38 +60,111 @@ function userAction(){
 
 function playRound(userChoice, computerChoice){
 
-    console.log(`User's Choice: ${userChoice}`);
-    console.log(`Computer's Choice: ${computerChoice}`);
+    const userPlay = document.createElement("p");
+    const compPlay = document.createElement("p");
+
+    userPlay.textContent = `User's Choice: ${userChoice}`;
+    userPlay.style.cssText = "color:blue; font-weight:bolder;";
+
+    compPlay.textContent = `Computer's Choice: ${computerChoice}`;
+    compPlay.style.cssText = "color:red; font-weight:bolder;";
+
+    display.appendChild(userPlay);
+    display.appendChild(compPlay);
 
     if(computerChoice === "scissors" && userChoice === "paper"){ 
-        console.log("Computer Wins");
         compWins++;
+        computerWins();
     }
     else if(computerChoice === "rock" && userChoice === "scissors"){
-        console.log("Computer Wins");
         compWins++;
+        computerWins();
     }
     else if(computerChoice === "paper" && userChoice === "rock"){ 
-        console.log("Computer Wins");
         compWins++;
+        computerWins();
     }
     else if(userChoice === "scissors" && computerChoice === "paper"){ 
-        console.log("User Wins");
         userWins++;
+        playerWins();
     }
     else if(userChoice === "paper" && computerChoice === "rock"){ 
-        console.log("User Wins");
         userWins++;
+        playerWins();
     }    
     else if(userChoice === "rock" && computerChoice === "scissors"){
-        console.log("User Wins");
         userWins++;
+        playerWins();
     }
     else
-        console.log("It's a draw"); 
+        draw();
 }
 
+function computerWins(){
 
+    const compDisplay = document.createElement("h1");
+    const results = document.createElement("h2");
+    //const userDisplay = document.createElement("h1");
+
+    compDisplay.textContent = "Computer Wins";
+    compDisplay.style.cssText = "color: red";
+
+    results.textContent = `User Wins: ${userWins} Comp Wins: ${compWins}`; 
+    
+    display.appendChild(compDisplay);
+    display.appendChild(results);
+
+
+}
+
+function playerWins(){
+
+    const userDisplay = document.createElement("h1");
+    const results = document.createElement("h2");
+
+    userDisplay.textContent = "User Wins";
+    userDisplay.style.cssText = "color: blue";
+
+    results.textContent = `User Wins: ${userWins} Comp Wins: ${compWins}`; 
+
+    display.appendChild(userDisplay);
+    display.appendChild(results);
+}
+
+function draw(){
+
+    const drawDisplay = document.createElement("h3");
+    const results = document.createElement("h2");
+
+    drawDisplay.textContent = "Its a draw";
+    drawDisplay.style.cssText = "color: green";
+
+    results.textContent = `User Wins: ${userWins} Comp Wins: ${compWins}`; 
+
+    display.appendChild(drawDisplay);
+    display.appendChild(results);
+
+}
+
+function playerVictory(){
+
+    const userVictory = document.createElement("h1");
+    userVictory.textContent = "User wins the WAR!!!!";
+    userVictory.style.cssText = "color:blue";
+
+    display.appendChild(userVictory);
+
+}
+
+function computerVictory(){
+
+    const compVictory = document.createElement("h1");
+    compVictory.textContent = "The day belongs to the MACHINES!!!!";
+    compVictory.style.cssText = "color:red";
+
+    display.appendChild(compVictory);
+
+}
 
 function game(){
     
@@ -98,39 +172,54 @@ function game(){
         userChoice = "rock";
         computerChoice = computerAction();
         playRound(userChoice, computerChoice);
-        console.log(`User Wins: ${userWins}`);
-        console.log(`Comp Wins: ${compWins}`);
         
-        if(userWins === 5)
-            console.log("User wins the WAR!!!!");
-        else if(compWins === 5)
-            console.log("The day belongs to the MACHINES!!!!");     
+        if(userWins === 5){
+            playerVictory();
+            userWins = 0;
+            compWins = 0;
+        }
+
+        else if(compWins === 5){
+            computerVictory();
+            userWins = 0;
+            compWins = 0;
+        }     
     });
 
     paper.addEventListener("click",()=>{
         userChoice = "paper";
         computerChoice = computerAction();
         playRound(userChoice, computerChoice);
-        console.log(`User Wins: ${userWins}`);
-        console.log(`Comp Wins: ${compWins}`);
 
-        if(userWins === 5)
-            console.log("User wins the WAR!!!!");
-        else if(compWins === 5)
-            console.log("The day belongs to the MACHINES!!!!"); 
+        if(userWins === 5){
+            playerVictory();
+            userWins = 0;
+            compWins = 0;
+        }
+
+        else if(compWins === 5){
+            computerVictory();
+            userWins = 0;
+            compWins = 0;
+        }    
     });
         
     scissors.addEventListener("click",()=>{
         userChoice = "scissors";
         computerChoice = computerAction();
         playRound(userChoice, computerChoice);
-        console.log(`User Wins: ${userWins}`);
-        console.log(`Comp Wins: ${compWins}`);
 
-        if(userWins === 5)
-            console.log("User wins the WAR!!!!");
-        else if(compWins === 5)
-            console.log("The day belongs to the MACHINES!!!!"); 
+        if(userWins === 5){
+            playerVictory();
+            userWins = 0;
+            compWins = 0;
+        }
+
+        else if(compWins === 5){
+            computerVictory();
+            userWins = 0;
+            compWins = 0;
+        }    
     });
 
 }
